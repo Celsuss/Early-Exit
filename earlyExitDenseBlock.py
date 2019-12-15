8d774458a35a663846d3a1f423bae39309aff60e
Jens Lordén <jenslorden@gmail.com>
2019-12-15 15:54:27 +0100

    Updated early exit model

--- earlyExitDenseBlock.py
+++ earlyExitDenseBlock.py
@@ -3,15 +3,19 @@ import tensorflow.keras as keras
 import tensorflow.keras.layers as layers
 
 class EarlyExitDenseBlock(keras.Model):
-    def __init__(self):
+    def __init__(self, size, activation='relu'):
         super(EarlyExitDenseBlock, self).__init__()
 
-        self.dense1 = layers.Dense(128)
+        self.dense1 = layers.Dense(size)
         self.dense2 = layers.Dense(10)
+        if activation == 'relu':
+            self.activation = tf.nn.relu
+        elif activation == 'softmax':
+            self.activation = tf.nn.softmax
 
     def call(self, input_tensor, training=False):
         x = self.dense1(input_tensor)
-        y1 = tf.nn.softmax(x)
-        x = tf.nn.relu(x)
-        x = self.dense2(x)
-        return tf.nn.softmax(x), y1
+        x = self.activation(x)
+        y = self.dense2(x)
+        y = tf.nn.softmax(y)
+        return x, y
--- earlyExitModel.py
+++ earlyExitModel.py
@@ -9,11 +9,13 @@ class EarlyExitModel(keras.Model):
         super(EarlyExitModel, self).__init__()
         self.conv1 = layers.Conv2D(32, 3, activation='relu')
         self.flatten = layers.Flatten()
-        self.ed1 = EarlyExitDenseBlock()
+        self.ed1 = EarlyExitDenseBlock(128)
+        self.ed2 = EarlyExitDenseBlock(10, activation='softmax')
 
     def call(self, x):
         x = self.conv1(x)
         x = self.flatten(x)
         x, y1 = self.ed1(x)
-        
+        x, y2 = self.ed2(x)
+        # x = keras.layers.concatenate([x, y1, y2])
         return x
